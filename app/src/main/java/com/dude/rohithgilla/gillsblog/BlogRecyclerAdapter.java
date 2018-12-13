a7aa96a8f87667e3f74cf715a1e681f67c28b20b
#Gilla_Dude! <gillarohith1@gmail.com>
2018-12-14 02:23:31 +0530

    Retrieved Image and put on home page!🤟
    Signed-off-by: #Gilla_Dude! <gillarohith1@gmail.com>

--- app/src/main/java/com/dude/rohithgilla/gillsblog/BlogRecyclerAdapter.java
+++ app/src/main/java/com/dude/rohithgilla/gillsblog/BlogRecyclerAdapter.java
@@ -1,17 +1,22 @@
 package com.dude.rohithgilla.gillsblog;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
 public class BlogRecyclerAdapter extends RecyclerView.Adapter<BlogRecyclerAdapter.ViewHolder> {
 
     public List<Blogpost> blogpostList;
+    public Context context;
     public BlogRecyclerAdapter(List<Blogpost> blogpostList){
         this.blogpostList = blogpostList;
     }
@@ -20,6 +25,7 @@ public class BlogRecyclerAdapter extends RecyclerView.Adapter<BlogRecyclerAdapte
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.blog_list_item,parent,false);
+        context = parent.getContext();
         return new ViewHolder(view);
     }
 
@@ -27,6 +33,9 @@ public class BlogRecyclerAdapter extends RecyclerView.Adapter<BlogRecyclerAdapte
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         String descData = blogpostList.get(position).getDesc();
         holder.setDescText(descData);
+
+        String image_url =blogpostList.get(position).getImage_url();
+        holder.setBlogImage(image_url);
     }
 
     @Override
@@ -37,6 +46,8 @@ public class BlogRecyclerAdapter extends RecyclerView.Adapter<BlogRecyclerAdapte
     public class ViewHolder extends RecyclerView.ViewHolder {
         private View mView;
         private TextView descView;
+        private ImageView blogImageView;
+
         public ViewHolder(View itemView) {
             super(itemView);
             mView = itemView;
@@ -44,6 +55,10 @@ public class BlogRecyclerAdapter extends RecyclerView.Adapter<BlogRecyclerAdapte
         public void setDescText(String descText){
             descView = mView.findViewById(R.id.blogPostDescription);
             descView.setText(descText);
+        }
+        public void setBlogImage(String downloadUri){
+            blogImageView = mView.findViewById(R.id.blogImage);
+            Glide.with(context).load(downloadUri).into(blogImageView);
         }
     }
 }
